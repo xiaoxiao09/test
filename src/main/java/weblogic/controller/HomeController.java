850f268205f2ca8850198e27dae668542059d3e7
xiaoxiao09 <676267900@qq.com>
2016-11-02 17:58:34 +0800

    first test

---
+++ src/main/java/weblogic/controller/HomeController.java
@@ -0,0 +1,16 @@
+package weblogic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+	@RequestMapping(value="/home")
+	public String home(){
+	    	
+		
+		return "hello";
+	}
+	
+}
